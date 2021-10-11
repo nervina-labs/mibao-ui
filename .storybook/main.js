e16a53cd27c6d134968407fff7900b78daed4c3d
INS <j893412899@outlook.com>
2021-10-11 18:29:22 +0800

    fix: Fixed storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,40 @@
+// module.exports = {
+//   stories: [],
+//   addons: ['@storybook/addon-essentials'],
+//   // uncomment the property below if you want to apply some webpack config globally
+//   // webpackFinal: async (config, { configType }) => {
+//   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+//
+//   //   // Return the altered config
+//   //   return config;
+//   // },
+// };
+
+const path = require("path")
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials'],
-  // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-
-  //   // Return the altered config
-  //   return config;
-  // },
-};
+  addons: [
+    "storybook-addon-performance/register",
+    "@storybook/addon-a11y",
+    "@storybook/addon-toolbars",
+  ],
+  typescript: {
+    reactDocgen: false,
+  },
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    }
+  },
+}
