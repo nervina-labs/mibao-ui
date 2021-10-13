1cbc40caabb378d636d7ae6992c60c531a2d3bb5
INS <j893412899@outlook.com>
2021-10-13 17:15:20 +0800

    chore: Add `jest-transform-stub`

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,7 @@ const { getJestProjects } = require('@nrwl/jest');
 
 module.exports = {
   projects: getJestProjects(),
+  moduleNameMapper: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+  }
 };
