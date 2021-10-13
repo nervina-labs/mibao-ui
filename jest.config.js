bebd77a489853205d2647f931ed5002134da38bd
INS <j893412899@outlook.com>
2021-10-13 17:45:38 +0800

    chore: Fixed svg unittest

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,6 @@ const { getJestProjects } = require('@nrwl/jest');
 module.exports = {
   projects: getJestProjects(),
   moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$": "jest-transform-stub"
   }
 };
