3618cb8ec7f52bdc187d3097f3f8bf2ded8d3bde
INS <j893412899@outlook.com>
2021-10-15 17:52:32 +0800

    fix: Fixed `theadVariant` type

--- apps/mibao-ui-docs/src/app/pages/table.stories.tsx
+++ apps/mibao-ui-docs/src/app/pages/table.stories.tsx
@@ -40,7 +40,7 @@ const args = {
   isNumeric: false,
   placement: 'bottom' as 'top' | 'bottom',
   variant: 'simple',
-  theadVariant: 'filled'
+  theadVariant: 'filled' as 'filled' | 'unstyled'
 }
 
 const Template: Story<typeof args> = (args) => <MibaoProvider>
