f0bbaf0b2c04534fb76f54ea58cb1cf967a9e6ce
INS <j893412899@outlook.com>
2021-10-20 14:45:29 +0800

    docs: Add skeleton docs

--- apps/mibao-ui-docs/src/app/pages/loading.stories.tsx
+++ apps/mibao-ui-docs/src/app/pages/loading.stories.tsx
@@ -1,6 +1,13 @@
 import { Story, Meta } from '@storybook/react'
 import { Stack } from '@chakra-ui/react'
-import { Loading as L, MibaoProvider } from 'mibao-ui'
+import {
+  Box,
+  Loading as L,
+  MibaoProvider,
+  SkeletonCircle as MibaoSkeletonCircle,
+  SkeletonText as MibaoSkeletonText,
+  Skeleton as MibaoSkeleton
+} from 'mibao-ui'
 
 export default {
   component: L,
@@ -36,4 +43,23 @@ export const SpinnerWithDifferentSize: Story = () => {
       </Stack>
     </MibaoProvider>
   )
+}
+
+export const Skeleton: Story = () => {
+  return <MibaoProvider>
+      <Stack>
+      <MibaoSkeleton height="20px" />
+      <MibaoSkeleton height="20px" />
+      <MibaoSkeleton height="20px" />
+    </Stack>
+  </MibaoProvider>
+}
+
+export const SkeletonText: Story = () => {
+  return <MibaoProvider>
+    <Box padding="6" boxShadow="lg" bg="white">
+      <MibaoSkeletonCircle size="10" />
+      <MibaoSkeletonText mt="4" noOfLines={4} spacing="4" />
+    </Box>
+  </MibaoProvider>
 }
