7130eabaa93ee7be42ca54d0101fffeee1c3afed
INS <j893412899@outlook.com>
2021-10-22 11:51:01 +0800

    feat: new tabs (#72)
    * feat: Add new tabs
    
    * fix: Fixed `onChange`
    
    * feat: Update tabs active line
    
    * fix: Variable name
    
    * fix: Fixed props omit

--- apps/mibao-ui-docs/src/app/pages/tab.stories.tsx
+++ apps/mibao-ui-docs/src/app/pages/tab.stories.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 import {
   MibaoProvider,
   Tab,
+  Tabs as MibaoUiTabs,
   TabList,
-  TabPanel,
   TabPanels,
-  Tabs as MibaoUiTabs
+  TabPanel
 } from 'mibao-ui'
 
 export default {
@@ -16,22 +16,164 @@ export default {
   }
 } as Meta
 
-const Template: Story = (args) => <MibaoProvider>
-  <MibaoUiTabs {...args}>
-    <TabList>
-      <Tab>One</Tab>
-      <Tab>Two</Tab>
-      <Tab>Three</Tab>
-    </TabList>
+export const LineTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
 
-    <TabPanels>
-      <TabPanel>1</TabPanel>
-      <TabPanel>2</TabPanel>
-      <TabPanel>3</TabPanel>
-    </TabPanels>
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
 
-  </MibaoUiTabs>
-</MibaoProvider>
+export const ColorsTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs colorScheme="black" {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
 
-export const Tabs = Template.bind({})
-Tabs.args = {}
+    <MibaoUiTabs colorScheme="gray" {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+
+    <MibaoUiTabs colorScheme="red" {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+
+    <MibaoUiTabs colorScheme="blue" {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+
+    <MibaoUiTabs colorScheme="yellow" {...args}>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
+
+export const EnclosedTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs variant="enclosed">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
+
+export const UnStyledTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs variant="unstyled">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
+
+export const DisabledTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs>
+      <TabList>
+        <Tab>One</Tab>
+        <Tab isDisabled>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
+
+export const SpaceBetweenTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs align="space-between">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+
+      <TabPanels>
+        <TabPanel>1</TabPanel>
+        <TabPanel>2</TabPanel>
+        <TabPanel>3</TabPanel>
+      </TabPanels>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
+
+export const SizeTabs: Story = (args) => {
+  return <MibaoProvider>
+    <MibaoUiTabs align="space-between" size="sm">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+
+    <MibaoUiTabs align="space-between">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+
+    <MibaoUiTabs align="space-between" size="lg">
+      <TabList>
+        <Tab>One</Tab>
+        <Tab>Two</Tab>
+        <Tab>Long long Long Three</Tab>
+      </TabList>
+    </MibaoUiTabs>
+  </MibaoProvider>
+}
