5d855edcde282c9d63c42b83632ab070a0a17051
INS <j893412899@outlook.com>
2021-10-28 18:48:46 +0800

    feat: Update `AspectRatio` containerProps (#88)
    * feat: Update `AspectRatio` containerProps
    
    * fix: Remove `Box`

--- apps/mibao-ui-docs/src/app/pages/image.stories.tsx
+++ apps/mibao-ui-docs/src/app/pages/image.stories.tsx
@@ -33,18 +33,20 @@ export default {
 
 const Template: Story = (args) =>
   <MibaoProvider>
-    <Stack spacing={3} direction="row">
+    <Stack spacing={3}>
       <MibaoImage {...args} />
       <MibaoImage {...args} loader={<Spinner color="primary.600" emptyColor="gray.200" m="auto" />} />
       <MibaoImage {...args} fallback={<div>Failed</div>} />
+
+      <h1>Ratio: 1:1: </h1>
+      <MibaoImage {...args} fallback={<div>Failed</div>} containerProps={{ ratio: 1 / 1 }} />
     </Stack>
   </MibaoProvider>
 
 export const Image = Template.bind({})
 Image.args = {
-  src: 'https://oss.jinse.cc/production/bd7c7508-026e-4fcd-abf8-1fa1061f4d6c.png',
+  src: 'https://oss.jinse.cc/production/e5a85cb5-bdd3-40c5-94de-c3335a704ab8.jpg',
   alt: '',
-  aspectRatio: true,
   width: '200px',
   height: '',
   objectFit: 'cover',
