0eae9310a85f51d3c96cb56f0f7642753e3353f4
INS <j893412899@outlook.com>
2021-11-02 13:52:26 +0800

    fix: Fixed img fallback not show

--- libs/mibao-ui/src/lib/image/image.tsx
+++ libs/mibao-ui/src/lib/image/image.tsx
@@ -89,7 +89,6 @@ export const Image: React.FC<ImageProps> = ({
         <ChakraImage
           position="relative"
           zIndex={1}
-          opacity={!imageSrc ? 0 : 1}
           {...props}
           src={imageSrc}
           className={`${styles.image} ${props.className ? props.className : ''}`}
