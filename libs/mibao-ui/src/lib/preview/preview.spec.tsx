48d9b9ddaf3a0e2547eeaee2206f64f52c9ba82a
INS <j893412899@outlook.com>
2021-11-08 15:54:51 +0800

    fix: fixed test

--- libs/mibao-ui/src/lib/preview/preview.spec.tsx
+++ libs/mibao-ui/src/lib/preview/preview.spec.tsx
@@ -4,7 +4,13 @@ import Preview from './preview'
 
 describe('Preview', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Preview isOpen={false} onClose={() => console.log('onClose')} renderer={''} />)
+    const { baseElement } = render(
+      <Preview
+        isOpen={false}
+        onClose={() => console.log('onClose')}
+        renderer={''}
+        render3D={renderer => <div>{renderer}</div>}
+      />)
     expect(baseElement).toBeTruthy()
   })
 })
