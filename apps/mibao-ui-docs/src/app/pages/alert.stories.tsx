dd6ee41953f40fb25b97908d5c21188ba34cd8ac
Keith <keithwhisper@gmail.com>
2021-11-11 17:56:02 +0800

    feat: allow alert to be plain style

--- apps/mibao-ui-docs/src/app/pages/alert.stories.tsx
+++ apps/mibao-ui-docs/src/app/pages/alert.stories.tsx
@@ -28,3 +28,22 @@ export const Alerts: Story<{ statusList: AlertStatus[] }> = ({
     </MibaoProvider>
   )
 }
+export const PlainAlerts: Story<{ statusList: AlertStatus[] }> = ({
+  statusList
+}) => {
+  return (
+    <MibaoProvider>
+      <Stack spacing={3}>
+        {statusList.map((status) => (
+          <Alert
+            isPlain
+            key={status}
+            status={status}
+            title={'Title'}
+            description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          />
+        ))}
+      </Stack>
+    </MibaoProvider>
+  )
+}
